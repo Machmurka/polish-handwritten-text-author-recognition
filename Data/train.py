f46076835c5eb9149d502a6f8d2c1509d6199d48
jakmachura <jakmachura@gmail.com>
2024-04-05 20:59:51 +0200

    Update train.py with Train function and documentation

--- Data/train.py
+++ Data/train.py
@@ -7,26 +7,58 @@ import torch
 import data_setup, engine, model_builder, Data.utils as utils
 from torchvision import transforms
 
-if __name__=='__main__':
-    
-    
+def Train(
+        data_procent:float,
+        num_epochs:int,
+        batch_size:int,
+        learning_rate:float,
+        block_num:list,
+        data_dir:str,
+        transform:transforms.Compose,
+        model_name:str
+):
+    '''
+        Train a pytorch model 
+
+        Args:
+            data_procent: Procent of data used for train and test 
+            num_epochs: Number of epochs
+            batch_size: Number of size of batch
+            learning_rate: Learning rate for optimizer
+            block_num: List with number of CONV block used in ResNet model
+            data_dir: Directory where data is stored
+            transform: Transformation used on data
+            model_name: file Name to save model
+
+        Example:
+            data_procent: 0.99  
+            num_epochs: 10 
+            batch_size: 32
+            learning_rate: 0.01
+            block_num: [3,4,6,3]
+            data_dir: r'Words'
+            transform: transforms.Compose([])
+            model_name: ResNet50.pth
+    '''
     print(os.getcwd())
 
     # Porcent wykorzystania całych danych
-    DATA_PROCENT=0.003
-    NUM_EPOCHS = 2
-    BATCH_SIZE = 32
-    LEARNING_RATE = 0.01
-    BLOCK_NUM=[3,4,6,3]
+    DATA_PROCENT=data_procent
+    NUM_EPOCHS = num_epochs
+    BATCH_SIZE = batch_size
+    LEARNING_RATE = learning_rate
+    BLOCK_NUM=block_num
     device = "cuda" if torch.cuda.is_available() else "cpu"
 
-    data_dir=r'Words'
+    data_dir=data_dir
     print(device)
+    
+    data_transform=transform
 
-    data_transform= transforms.Compose([transforms.Resize(size=(224,224)),
-                    transforms.TrivialAugmentWide(num_magnitude_bins=31),
-                    transforms.ToTensor()
-                    ])
+    # data_transform= transforms.Compose([transforms.Resize(size=(224,224)),
+    #                 transforms.TrivialAugmentWide(num_magnitude_bins=31),
+    #                 transforms.ToTensor()
+    #                 ])
     
     train_dataloader, test_dataloader, class_names = data_setup.create_dataloaders(
         DatasetDir=data_dir,
@@ -57,7 +89,8 @@ if __name__=='__main__':
 
     utils.save_model(model=model,
                  target_dir="models",
-                 model_name="ResNet50-Transform224x224-NoShedule-NoDropOut.pth")
+                 model_name=model_name)
     
+    # "ResNet50-Transform224x224-NoShedule-NoDropOut.pth"
 
     engine.plot_loss_curves(results)
